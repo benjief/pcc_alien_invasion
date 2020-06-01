3ed7a38d64d63dea85d5d264db22ac6f58e87cb3
benjief <benjiefriedman@gmail.com>
2020-05-31 22:59:48 -0700

    Added L & R movement controls to ship.

--- ship.py
+++ ship.py
@@ -8,6 +8,8 @@ class Ship:
 		self.screen = ai_game.screen
 		self.screen_rect = ai_game.screen.get_rect()
 
+		#self.settings = ai_game.settings
+
 		# Load the ship image and get its rect.
 		self.image = pygame.image.load('images/ship_small.png')
 		self.rect = self.image.get_rect()
@@ -15,6 +17,19 @@ class Ship:
 		# Start each new ship at the bottom centre of the screen.
 		self.rect.midbottom = self.screen_rect.midbottom
 
+		# Movement flags
+		self.moving_right = False
+		self.moving_left = False
+
 	def blitme(self):
 		"""Draw the ship at its current location."""
 		self.screen.blit(self.image, self.rect)
+
+	def update(self):
+		"""Update the ship's position based on movement flags."""
+		if (self.moving_right and 
+				self.rect.bottomright[0] <= self.screen_rect.bottomright[0]):
+			self.rect.x += 1
+		if (self.moving_left and
+				self.rect.bottomleft[0] >= self.screen_rect.bottomleft[0]):
+			self.rect.x -= 1
