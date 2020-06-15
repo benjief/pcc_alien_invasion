4a4bb227270d422cfbbb5babe66955f03eaaf66d
benjief <benjiefriedman@gmail.com>
2020-06-15 16:31:19 -0700

    Bullets disappear when off the screen in alien_invasion.py.

--- exercises/rocket_settings.py
+++ exercises/rocket_settings.py
@@ -27,7 +27,7 @@ class RocketSettings:
 		self.rotation_angle = 0
 
 		# Bullet settings
-		self.bullet_speed = 1.0
+		self.bullet_speed = 3.0
 		self.bullet_width = 3
 		self.bullet_height = 15
 		self.bullet_color = (60, 60, 60)
